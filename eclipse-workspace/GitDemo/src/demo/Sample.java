8337b0eeefa71f7fc158a7fdb91eeb466e41d93e
ArnavAt3 <bmsit.arnav.ise@gmail.com>
2025-01-29 15:40:02 +0530

    Edited Sample.java

--- eclipse-workspace/GitDemo/src/demo/Sample.java
+++ eclipse-workspace/GitDemo/src/demo/Sample.java
@@ -4,7 +4,9 @@ public class Sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Hell");
+        for(int i=0;i<5;i++) {
+        	return;
+        }
 	}
 
 }
