4ba53eaa8ad00fe6ae154798db4f842fd49ff65e
ArnavAt3 <bmsit.arnav.ise@gmail.com>
2025-01-29 17:11:47 +0530

    Updated Sample

--- eclipse-workspace/GitDemo/src/demo/Sample.java
+++ eclipse-workspace/GitDemo/src/demo/Sample.java
@@ -5,7 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
         for(int i=0;i<5;i++) {
-        	return;
+        	System.out.println("Iteration: " + i);
         }
 	}
 
